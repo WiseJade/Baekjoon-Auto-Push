e1cb232343bbf8a1c99e5d8fae2e676b3447d533
WiseJade <104193237+WiseJade@users.noreply.github.com>
2022-05-26 10:20:24 +0900

    [Bronze III] Title: 직사각형에서 탈출, Time: 204 ms, Memory: 17636 KB -BaekjoonHub

--- 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.java
+++ 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.java
@@ -8,18 +8,6 @@ public class Main {
         int w = sc.nextInt();
         int h = sc.nextInt();
 
-        int horizon, length;
-
-        if((double)x < (double)w / 2) {
-            horizon = x;
-        }
-        else horizon = w - x;
-
-        if((double)y < (double)h / 2) {
-            length = y;
-        }
-        else length = h - y;
-
-        System.out.print(Math.min(horizon, length));
+        System.out.println(Math.min((Math.min(Math.min(w-x, h-y),y)),x));
     }
 }
