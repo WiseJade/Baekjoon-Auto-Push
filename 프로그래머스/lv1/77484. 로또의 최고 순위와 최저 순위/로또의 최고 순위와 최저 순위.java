b55171e26240409e040deb249c01bead389f56da
WiseJade <104193237+WiseJade@users.noreply.github.com>
2022-08-01 16:30:39 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.03 ms, Memory: 77.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int[] solution(int[] lottos, int[] win_nums) {
+        int[] answer = new int[2];
+        int lowCount = 0;
+        int highCount = 0;
+        for (int i = 0; i < lottos.length; i++) {
+            if (lottos[i] == 0)
+                highCount++;
+            for (int j = 0; j < win_nums.length; j++) {
+                if (lottos[i] == win_nums[j]) {
+                    highCount++;
+                    lowCount++;
+                }
+            }
+        }
+        answer[0] = ranking(highCount);
+        answer[1] = ranking(lowCount);
+        return answer;
+    }
+    public int ranking(int score) {
+        switch(score) {
+            case 6:
+                return 1;
+            case 5:
+                return 2;
+            case 4:
+                return 3;
+            case 3:
+                return 4;
+            case 2:
+                return 5;
+            default:
+                return 6;
+        }
+    }
+}
