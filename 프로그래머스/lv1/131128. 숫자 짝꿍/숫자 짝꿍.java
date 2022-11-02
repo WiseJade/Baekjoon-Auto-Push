cea8b2440a9764790001837dd0d896b41c7b0f60
WiseJade <jeonhr861@gmail.com>
2022-11-02 20:22:01 +0900

    [level 1] Title: 숫자 짝꿍, Time: 0.61 ms, Memory: 75.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/131128. 숫자 짝꿍/숫자 짝꿍.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+import java.lang.StringBuffer;
+
+class Solution {
+    public String solution(String x, String y) {
+        StringBuffer answer = new StringBuffer("");
+        char[] xArr = x.toCharArray();
+        char[] yArr = y.toCharArray();
+        Arrays.sort(xArr);
+        Arrays.sort(yArr);
+        int xIdx = 0;
+        int yIdx = 0;
+        while (xIdx < xArr.length && yIdx < yArr.length) {
+            if (xArr[xIdx] == yArr[yIdx]) {
+                answer.append(String.valueOf(xArr[xIdx]));
+                xIdx++;
+                yIdx++;
+            } else if (xArr[xIdx] < yArr[yIdx]) {
+                xIdx++;
+            } else if (xArr[xIdx] > yArr[yIdx]) {
+                yIdx++;
+            }
+        }
+        if (answer.length() < 1)    return "-1";
+        answer.reverse();
+        String result = answer.toString();
+        if (!result.contains("1") && !result.contains("2") && !result.contains("3") &&
+           !result.contains("4") && !result.contains("5") && !result.contains("6") &&
+           !result.contains("7") && !result.contains("8") && !result.contains("9")) {
+            return "0";
+        }
+        return result;
+        // return String.valueOf(Long.parseLong(result));
+    }
+}
